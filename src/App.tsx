81ba5afff682ecf825e8c895c292256d8cbb7c01
Seungrok <sonicrok93@gmail.com>
2023-08-16 03:39:27 +0900

    feat: add page layout component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,42 @@
 import { Outlet } from 'react-router-dom'
 import NavBar from './components/NavBar'
+import PageLayout from './pages/PageLayout'
+import styled from '@emotion/styled'
 
 function App() {
   return (
-    <div className="App">
+    <Wrapper className="App">
       <NavBar />
-      {"Welcome to Seungrok's Todo!"}
-      <main>
+      <PageLayout>
         <Outlet />
-      </main>
-    </div>
+      </PageLayout>
+      <Footer>
+        <Span>2023 Wanted Pre-onboarding Mission</Span>
+        <Span>by Yoon Seungrok</Span>
+      </Footer>
+    </Wrapper>
   )
 }
 
 export default App
+
+const Wrapper = styled.div`
+  height: 100%;
+`
+
+const Footer = styled.footer`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 5rem;
+  margin-top: 50px;
+  padding: 10px auto;
+`
+
+const Span = styled.span`
+  font-size: 1rem;
+  text-align: center;
+  vertical-align: middle;
+  line-height: 1.5;
+`
