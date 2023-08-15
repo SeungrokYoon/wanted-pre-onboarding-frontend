19bd12fad4f594349e4f4ba5593ea805bb44c0e6
Seungrok <sonicrok93@gmail.com>
2023-08-16 03:36:59 +0900

    style: change style

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,14 +2,14 @@ import { Link } from 'react-router-dom'
 
 const HomePage = () => {
   return (
-    <div>
-      HomePage
+    <section>
+      {"Welcome to Seungrok's Todo!"}
       <div>
         <button type="button">
           <Link to="/todo">TodoList 구경하러 가기</Link>
         </button>
       </div>
-    </div>
+    </section>
   )
 }
 
