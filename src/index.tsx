283c42078e93ee59f106fd6cee9ab2e89a8e290b
Seungrok <sonicrok93@gmail.com>
2023-08-16 00:48:38 +0900

    refactor: change to nested routing

--- src/index.tsx
+++ src/index.tsx
@@ -3,19 +3,26 @@ import ReactDOM from 'react-dom/client'
 import reportWebVitals from './reportWebVitals'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import App from './App'
+import HomePage from './pages/HomePage'
 import SignUpPage from './pages/SignUpPage'
 import SignInPage from './pages/SignInPage'
 import TodoPage from './pages/TodoPage'
 import { AuthProvider } from './AuthProvider'
 
 const router = createBrowserRouter([
-  { path: '/', element: <App /> },
   {
-    path: '/signup',
-    element: <SignUpPage />,
+    path: '/',
+    element: <App />,
+    children: [
+      { index: true, element: <HomePage /> },
+      {
+        path: 'signup',
+        element: <SignUpPage />,
+      },
+      { path: 'signin', element: <SignInPage /> },
+      { path: 'todo', element: <TodoPage /> },
+    ],
   },
-  { path: '/signin', element: <SignInPage /> },
-  { path: '/todo', element: <TodoPage /> },
 ])
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
