587d251c7956825184d56ae70723974991121790
Seungrok <sonicrok93@gmail.com>
2023-08-27 13:52:35 +0900

    refactor: redefine axios instance and get, post function with generic

---
+++ src/api/auth.ts
@@ -0,0 +1,38 @@
+import { api } from '../utils/instance'
+
+const AUTH_API_URL = {
+  signup: '/auth/signup',
+  signin: '/auth/signin',
+} as const
+
+type SignupBody = { email: string; password: string }
+type SigninBody = { email: string; password: string }
+type AuthBodyType = SignupBody | SigninBody
+type SignupResponse = null
+type SigninResponse = {
+  access_token: string
+}
+
+export const signupRequest = async ({ email, password }: AuthBodyType) => {
+  return await api.post<SignupResponse, AuthBodyType>(
+    AUTH_API_URL.signup,
+    {
+      email,
+      password,
+    },
+    { validateStatus: (status) => status < 300 }
+  )
+}
+
+export const signinRequest = async ({ email, password }: AuthBodyType) => {
+  return await api.post<SigninResponse, AuthBodyType>(
+    AUTH_API_URL.signin,
+    {
+      email,
+      password,
+    },
+    { validateStatus: (status) => status < 500 }
+  )
+}
+
+export type { SigninResponse }
---
+++ src/utils/instance.ts
@@ -0,0 +1,25 @@
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios'
+
+const BASE_API_URL = 'https://www.pre-onboarding-selection-task.shop'
+const TOKEN_KEY_STR = 'access_token'
+const withBearer = (tokenStr: string) => `Bearer ${tokenStr}`
+
+const axiosInstance = axios.create({
+  baseURL: BASE_API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
+
+axios.interceptors.request.use(function (config) {
+  const token = localStorage.getItem(TOKEN_KEY_STR)
+  if (token !== null) config.headers.Authorization = withBearer(token)
+  return config
+})
+
+export const api = {
+  get: <T, D>(url: string, config?: AxiosRequestConfig) =>
+    axiosInstance.get<T, AxiosResponse<T, D>, D>(url, config),
+  post: <T, D>(url: string, data: D, config?: AxiosRequestConfig) =>
+    axiosInstance.post<T, AxiosResponse<T, D>, D>(url, data, config),
+}
